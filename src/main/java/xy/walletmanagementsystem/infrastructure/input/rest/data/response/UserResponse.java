50cae6c2d362deea0ba91c6a0ba22ea73abdb9eb
deji <ayodeji2k@gmail.com>
2026-04-17 16:45:04 +0100

    create userResponse

---
+++ src/main/java/xy/walletmanagementsystem/infrastructure/input/rest/data/response/UserResponse.java
@@ -0,0 +1,24 @@
+package xy.walletmanagementsystem.infrastructure.input.rest.data.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import xy.walletmanagementsystem.domain.enums.AccountStatus;
+import xy.walletmanagementsystem.domain.enums.UserRole;
+
+import java.time.LocalDateTime;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class UserResponse {
+    private String id;
+    private String fullName;
+    private String email;
+    private String phoneNumber;
+    private AccountStatus status;
+    private UserRole role;
+    private LocalDateTime createdDate;
+}
