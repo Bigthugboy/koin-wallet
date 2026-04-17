3fd0b0f554f4d6617f9555385b9e947d1f6e9ae8
deji <ayodeji2k@gmail.com>
2026-04-17 13:25:10 +0100

    created AuthResponse

---
+++ src/main/java/xy/walletmanagementsystem/domain/model/AuthResponse.java
@@ -0,0 +1,21 @@
+package xy.walletmanagementsystem.domain.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class AuthResponse {
+    private String accessToken;
+    private String refreshToken;
+    @Builder.Default
+    private String type = "Bearer";
+    private LocalDateTime issuedAt;
+    private LocalDateTime expiresAt;
+}
