1f0f3e983bfa02cb498fc01ea7d4b03b80213d77
deji <ayodeji2k@gmail.com>
2026-04-17 16:44:18 +0100

    create userEntities

---
+++ src/main/java/xy/walletmanagementsystem/infrastructure/output/entities/UserEntity.java
@@ -0,0 +1,46 @@
+package xy.walletmanagementsystem.infrastructure.output.entities;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import xy.walletmanagementsystem.domain.enums.AccountStatus;
+import xy.walletmanagementsystem.domain.enums.UserRole;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "users")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class UserEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private String id;
+
+    @Column(nullable = false)
+    private String fullName;
+
+    @Column(unique = true, nullable = false)
+    private String email;
+
+    @Column(unique = true, nullable = false)
+    private String phoneNumber;
+
+    @Column(nullable = false)
+    private String passwordHash;
+
+    @Enumerated(EnumType.STRING)
+    private AccountStatus status;
+
+    @Enumerated(EnumType.STRING)
+    private UserRole role;
+
+    private LocalDateTime createdDate;
+    private LocalDateTime updatedDate;
+}
