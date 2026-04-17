4fc2a4d0e920703f28cc05a597c8533c360132ef
deji <ayodeji2k@gmail.com>
2026-04-17 18:33:42 +0100

    update method

--- src/main/java/xy/walletmanagementsystem/domain/service/UserService.java
+++ src/main/java/xy/walletmanagementsystem/domain/service/UserService.java
@@ -4,9 +4,12 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import xy.walletmanagementsystem.applicationPort.input.UserUseCase;
 import xy.walletmanagementsystem.applicationPort.output.UserOutPutPort;
+import xy.walletmanagementsystem.domain.exception.WalletManagementException;
 import xy.walletmanagementsystem.domain.model.User;
+import xy.walletmanagementsystem.infrastructure.input.rest.message.ErrorMessages;
 
 import java.time.LocalDateTime;
+import java.util.logging.ErrorManager;
 
 @Service
 @RequiredArgsConstructor
@@ -20,9 +23,9 @@ public class UserService implements UserUseCase {
     }
 
     @Override
-    public User updateUserProfile(String userId, User userDetails) {
+    public User updateUserProfile(String userId, User userDetails) throws WalletManagementException {
         User existingUser = userOutPutPort.findById(userId)
-                .orElseThrow(() -> new RuntimeException("User not found"));
+                .orElseThrow(() -> new WalletManagementException(ErrorMessages.USER_NOT_FOUND));
 
         if (userDetails.getFullName() != null) {
             existingUser.setFullName(userDetails.getFullName());
@@ -30,7 +33,6 @@ public class UserService implements UserUseCase {
         if (userDetails.getPhoneNumber() != null) {
             existingUser.setPhoneNumber(userDetails.getPhoneNumber());
         }
-
         existingUser.setUpdatedDate(LocalDateTime.now());
         return userOutPutPort.save(existingUser);
     }
