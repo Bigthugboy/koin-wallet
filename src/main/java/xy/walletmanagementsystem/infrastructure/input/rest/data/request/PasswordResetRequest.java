3383837d3f0d5152df0bb4ad8dd828612f111dee
deji <ayodeji2k@gmail.com>
2026-04-17 16:41:03 +0100

    created passwordResetRequest

---
+++ src/main/java/xy/walletmanagementsystem/infrastructure/input/rest/data/request/PasswordResetRequest.java
@@ -0,0 +1,24 @@
+package xy.walletmanagementsystem.infrastructure.input.rest.data.request;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class PasswordResetRequest {
+    @Email(message = "Invalid email format")
+    @NotBlank(message = "Email is required")
+    private String email;
+
+    @NotBlank(message = "OTP is required")
+    private String otp;
+
+    @NotBlank(message = "New password is required")
+    private String newPassword;
+}
