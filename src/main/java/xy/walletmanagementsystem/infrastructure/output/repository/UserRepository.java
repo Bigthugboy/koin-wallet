5d0bb453896db1c39a1b2bc66cd6c7ba1ec22a39
deji <ayodeji2k@gmail.com>
2026-04-17 14:10:53 +0100

    created UserRepository

---
+++ src/main/java/xy/walletmanagementsystem/infrastructure/output/repository/UserRepository.java
@@ -0,0 +1,10 @@
+package xy.walletmanagementsystem.infrastructure.output.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import xy.walletmanagementsystem.domain.model.User;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, String> {
+    Optional<User> findByEmail(String email);
+}
