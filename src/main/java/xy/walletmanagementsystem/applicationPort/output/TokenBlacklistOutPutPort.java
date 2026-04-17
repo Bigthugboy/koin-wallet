5fd3ebd3c80b17ea7f71cf590d9ebe3ccf5aeafe
deji <ayodeji2k@gmail.com>
2026-04-17 18:13:32 +0100

    added blacklistTOken

--- src/main/java/xy/walletmanagementsystem/applicationPort/output/TokenBlacklistOutPutPort.java
+++ src/main/java/xy/walletmanagementsystem/applicationPort/output/TokenBlacklistOutPutPort.java
@@ -2,4 +2,6 @@ package xy.walletmanagementsystem.applicationPort.output;
 
 public interface TokenBlacklistOutPutPort {
     boolean isTokenBlacklisted(String jwt);
+
+    void blacklistToken(String token, long remainingTimeMs);
 }
