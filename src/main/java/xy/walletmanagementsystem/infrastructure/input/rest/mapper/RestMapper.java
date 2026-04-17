958940797d89696a2908ef9f721d0287f0c17f82
deji <ayodeji2k@gmail.com>
2026-04-17 19:33:21 +0100

    created restMapper

---
+++ src/main/java/xy/walletmanagementsystem/infrastructure/input/rest/mapper/RestMapper.java
@@ -0,0 +1,32 @@
+package xy.walletmanagementsystem.infrastructure.input.rest.mapper;
+
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import xy.walletmanagementsystem.domain.model.AuthResponse;
+import xy.walletmanagementsystem.domain.model.Kyc;
+import xy.walletmanagementsystem.domain.model.Loan;
+import xy.walletmanagementsystem.domain.model.Transaction;
+import xy.walletmanagementsystem.domain.model.User;
+import xy.walletmanagementsystem.domain.model.Wallet;
+import xy.walletmanagementsystem.infrastructure.input.rest.data.response.AuthenticationResponse;
+import xy.walletmanagementsystem.infrastructure.input.rest.data.response.KycResponse;
+import xy.walletmanagementsystem.infrastructure.input.rest.data.response.LoanResponse;
+import xy.walletmanagementsystem.infrastructure.input.rest.data.response.TransactionResponse;
+import xy.walletmanagementsystem.infrastructure.input.rest.data.response.UserResponse;
+import xy.walletmanagementsystem.infrastructure.input.rest.data.response.WalletResponse;
+
+@Mapper(componentModel = "spring")
+public interface RestMapper {
+
+    UserResponse toResponse(User user);
+
+    WalletResponse toResponse(Wallet wallet);
+
+    LoanResponse toResponse(Loan loan);
+
+    TransactionResponse toResponse(Transaction transaction);
+
+    KycResponse toResponse(Kyc kyc);
+
+    AuthenticationResponse toResponse(AuthResponse authResponse);
+}
