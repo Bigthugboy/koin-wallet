c37210d8704fa592264d8da0b35fd3d2b1636cda
deji <ayodeji2k@gmail.com>
2026-04-17 16:34:41 +0100

    create kycEntity class

---
+++ src/main/java/xy/walletmanagementsystem/infrastructure/output/entities/KycEntity.java
@@ -0,0 +1,39 @@
+package xy.walletmanagementsystem.infrastructure.output.entities;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import xy.walletmanagementsystem.domain.enums.KycVerificationStatus;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "kyc_details")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class KycEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private String id;
+
+    @Column(nullable = false, unique = true)
+    private String userId;
+
+    @Column(unique = true)
+    private String bvn;
+
+    @Column(unique = true)
+    private String nin;
+
+    @Enumerated(EnumType.STRING)
+    private KycVerificationStatus status;
+
+    private LocalDateTime createdDate;
+    private LocalDateTime updatedDate;
+}
