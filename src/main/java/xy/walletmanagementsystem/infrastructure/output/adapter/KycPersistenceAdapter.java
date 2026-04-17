be6494a54afb0b8b25e9195e2a4962fbd614ecfe
deji <ayodeji2k@gmail.com>
2026-04-17 16:35:16 +0100

    create KycPersistenceAdapter

---
+++ src/main/java/xy/walletmanagementsystem/infrastructure/output/adapter/KycPersistenceAdapter.java
@@ -0,0 +1,37 @@
+package xy.walletmanagementsystem.infrastructure.output.adapter;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+import xy.walletmanagementsystem.applicationPort.output.KycOutPutPort;
+import xy.walletmanagementsystem.domain.model.Kyc;
+import xy.walletmanagementsystem.infrastructure.output.entities.KycEntity;
+import xy.walletmanagementsystem.infrastructure.output.mapper.KycMapper;
+import xy.walletmanagementsystem.infrastructure.output.repository.KycRepository;
+
+import java.util.Optional;
+
+@Component
+@RequiredArgsConstructor
+public class KycPersistenceAdapter implements KycOutPutPort {
+    private final KycRepository kycRepository;
+    private final KycMapper kycMapper;
+
+    @Override
+    public Kyc save(Kyc kyc) {
+        KycEntity entity = kycMapper.toEntity(kyc);
+        KycEntity savedEntity = kycRepository.save(entity);
+        return kycMapper.toDomain(savedEntity);
+    }
+
+    @Override
+    public Optional<Kyc> findByUserId(String userId) {
+        return kycRepository.findByUserId(userId)
+                .map(kycMapper::toDomain);
+    }
+
+    @Override
+    public Optional<Kyc> findById(String kycId) {
+        return kycRepository.findById(kycId)
+                .map(kycMapper::toDomain);
+    }
+}
