ce213f04e072cf37db8e49c0a7a0d6c23a145b61
deji <ayodeji2k@gmail.com>
2026-04-17 16:44:55 +0100

    created userPersistenceAdapter

---
+++ src/main/java/xy/walletmanagementsystem/infrastructure/output/adapter/UserPersistenceAdapter.java
@@ -0,0 +1,36 @@
+package xy.walletmanagementsystem.infrastructure.output.adapter;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+import xy.walletmanagementsystem.applicationPort.output.UserOutPutPort;
+import xy.walletmanagementsystem.domain.model.User;
+import xy.walletmanagementsystem.infrastructure.output.entities.UserEntity;
+import xy.walletmanagementsystem.infrastructure.output.mapper.UserMapper;
+import xy.walletmanagementsystem.infrastructure.output.repository.UserRepository;
+
+import java.util.Optional;
+
+@Component
+@RequiredArgsConstructor
+public class UserPersistenceAdapter implements UserOutPutPort {
+    private final UserRepository userRepository;
+    private final UserMapper userMapper;
+
+    @Override
+    public Optional<User> findByEmail(String email) {
+        return userRepository.findByEmail(email)
+                .map(userMapper::toDomain);
+    }
+
+    @Override
+    public Optional<User> findById(String userId) {
+        return userRepository.findById(userId)
+                .map(userMapper::toDomain);
+    }
+
+    public User save(User user) {
+        UserEntity entity = userMapper.toEntity(user);
+        UserEntity savedEntity = userRepository.save(entity);
+        return userMapper.toDomain(savedEntity);
+    }
+}
