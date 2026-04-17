765d7d018f2a5a67c8a1fad9dba0aeb6cb9015b7
deji <ayodeji2k@gmail.com>
2026-04-17 16:48:59 +0100

    update user fields

--- src/main/java/xy/walletmanagementsystem/domain/model/User.java
+++ src/main/java/xy/walletmanagementsystem/domain/model/User.java
@@ -1,8 +1,11 @@
 package xy.walletmanagementsystem.domain.model;
 
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
+import xy.walletmanagementsystem.domain.enums.AccountStatus;
 import xy.walletmanagementsystem.domain.enums.UserRole;
 
 import java.time.LocalDateTime;
@@ -10,21 +13,16 @@ import java.time.LocalDateTime;
 @Getter
 @Setter
 @AllArgsConstructor
+@NoArgsConstructor
+@Builder
 public class User {
     private String id;
-    private String firstName;
-    private String lastName;
+    private String fullName;
     private String email;
+    private String phoneNumber;
     private String passwordHash;
+    private AccountStatus status;
     private UserRole role;
-    private LocalDateTime dateCreated;
-    private LocalDateTime dateUpdated;
-
+    private LocalDateTime createdDate;
+    private LocalDateTime updatedDate;
 }
-//Full Name — String
-//• Email Address — String (Unique)
-//• Phone Number — String (Unique)
-//• Password — Encrypted String
-//• BVN/NIN (Mocked) — String
-//• Account Status — Integer (Active, Suspended, Pending)
-//• Created Date — Timestamp
