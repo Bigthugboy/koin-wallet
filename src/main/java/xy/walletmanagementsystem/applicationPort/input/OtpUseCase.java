c6606173ff63fce022a73d1bf31fba69db559544
deji <ayodeji2k@gmail.com>
2026-04-17 13:26:09 +0100

    created otpUsecase

---
+++ src/main/java/xy/walletmanagementsystem/applicationPort/input/OtpUseCase.java
@@ -0,0 +1,12 @@
+package xy.walletmanagementsystem.applicationPort.input;
+
+import xy.walletmanagementsystem.domain.OtpType;
+import xy.walletmanagementsystem.domain.exception.WalletManagementException;
+import xy.walletmanagementsystem.domain.model.OtpDetails;
+
+public interface OtpUseCase {
+    OtpDetails generateOtp(String email, OtpType type) throws WalletManagementException;
+    boolean verifyOtp(String email, String otp, OtpType type) throws WalletManagementException;
+    OtpDetails resendOtp(String email, OtpType type) throws WalletManagementException;
+
+}
