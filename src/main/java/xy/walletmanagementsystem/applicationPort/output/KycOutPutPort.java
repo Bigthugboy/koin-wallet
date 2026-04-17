c9c08de520b192f6084b052ad04e6bd2a22408c9
deji <ayodeji2k@gmail.com>
2026-04-17 16:35:02 +0100

    create kycOutPutPort

---
+++ src/main/java/xy/walletmanagementsystem/applicationPort/output/KycOutPutPort.java
@@ -0,0 +1,11 @@
+package xy.walletmanagementsystem.applicationPort.output;
+
+import xy.walletmanagementsystem.domain.model.Kyc;
+
+import java.util.Optional;
+
+public interface KycOutPutPort {
+    Kyc save(Kyc kyc);
+    Optional<Kyc> findByUserId(String userId);
+    Optional<Kyc> findById(String kycId);
+}
