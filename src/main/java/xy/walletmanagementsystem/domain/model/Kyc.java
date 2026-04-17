5031bc6240be58b1b2e986285826c680c777999a
deji <ayodeji2k@gmail.com>
2026-04-17 16:34:18 +0100

    create kyc class

---
+++ src/main/java/xy/walletmanagementsystem/domain/model/Kyc.java
@@ -0,0 +1,25 @@
+package xy.walletmanagementsystem.domain.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import xy.walletmanagementsystem.domain.enums.KycVerificationStatus;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class Kyc {
+    private String id;
+    private String userId;
+    private String bvn;
+    private String nin;
+    private KycVerificationStatus status;
+    private LocalDateTime createdDate;
+    private LocalDateTime updatedDate;
+}
