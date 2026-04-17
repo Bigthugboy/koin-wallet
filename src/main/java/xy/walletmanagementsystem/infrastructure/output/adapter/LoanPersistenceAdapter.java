03b689437d911f78e3f9483d6c010f852a7727da
deji <ayodeji2k@gmail.com>
2026-04-17 16:38:01 +0100

    create loanPersistenceAdapter

---
+++ src/main/java/xy/walletmanagementsystem/infrastructure/output/adapter/LoanPersistenceAdapter.java
@@ -0,0 +1,47 @@
+package xy.walletmanagementsystem.infrastructure.output.adapter;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+import xy.walletmanagementsystem.applicationPort.output.LoanOutPutPort;
+import xy.walletmanagementsystem.domain.model.Loan;
+import xy.walletmanagementsystem.infrastructure.output.entities.LoanEntity;
+import xy.walletmanagementsystem.infrastructure.output.mapper.LoanMapper;
+import xy.walletmanagementsystem.infrastructure.output.repository.LoanRepository;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
+
+@Component
+@RequiredArgsConstructor
+public class LoanPersistenceAdapter implements LoanOutPutPort {
+    private final LoanRepository loanRepository;
+    private final LoanMapper loanMapper;
+
+    @Override
+    public Loan save(Loan loan) {
+        LoanEntity entity = loanMapper.toEntity(loan);
+        LoanEntity savedEntity = loanRepository.save(entity);
+        return loanMapper.toDomain(savedEntity);
+    }
+
+    @Override
+    public Optional<Loan> findById(String loanId) {
+        return loanRepository.findById(loanId)
+                .map(loanMapper::toDomain);
+    }
+
+    @Override
+    public List<Loan> findByUserId(String userId) {
+        return loanRepository.findByUserId(userId).stream()
+                .map(loanMapper::toDomain)
+                .collect(Collectors.toList());
+    }
+
+    @Override
+    public List<Loan> findAll() {
+        return loanRepository.findAll().stream()
+                .map(loanMapper::toDomain)
+                .collect(Collectors.toList());
+    }
+}
