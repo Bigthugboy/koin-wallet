f1fef782244d140e02ac8956e91d0856a92e7d27
deji <ayodeji2k@gmail.com>
2026-04-17 16:42:05 +0100

    created transactionEntity

---
+++ src/main/java/xy/walletmanagementsystem/infrastructure/output/entities/TransactionEntity.java
@@ -0,0 +1,47 @@
+package xy.walletmanagementsystem.infrastructure.output.entities;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import xy.walletmanagementsystem.domain.enums.TransactionStatus;
+import xy.walletmanagementsystem.domain.enums.TransactionType;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "transactions")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class TransactionEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private String id;
+
+    @Column(nullable = false)
+    private String userId;
+
+    @Column(nullable = false)
+    private String walletId;
+
+    @Enumerated(EnumType.STRING)
+    private TransactionType type;
+
+    @Column(nullable = false, precision = 19, scale = 2)
+    private BigDecimal amount;
+
+    @Enumerated(EnumType.STRING)
+    private TransactionStatus status;
+
+    @Column(nullable = false, unique = true)
+    private String referenceNumber;
+
+    @Column(nullable = false)
+    private LocalDateTime timestamp;
+}
