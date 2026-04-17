0a05f754f416c20ca7cf9d1d2cd4ce046bdefc6c
deji <ayodeji2k@gmail.com>
2026-04-17 16:48:28 +0100

    add LoanUseCase

--- src/main/java/xy/walletmanagementsystem/applicationPort/input/LoanUseCase.java
+++ src/main/java/xy/walletmanagementsystem/applicationPort/input/LoanUseCase.java
@@ -1,4 +1,17 @@
 package xy.walletmanagementsystem.applicationPort.input;
 
+import xy.walletmanagementsystem.domain.exception.WalletManagementException;
+import xy.walletmanagementsystem.domain.model.Loan;
+
+import java.math.BigDecimal;
+import java.util.List;
+
 public interface LoanUseCase {
+    Loan applyForLoan(String userId, BigDecimal amount, Integer durationInDays) throws WalletManagementException;
+    Loan approveLoan(String loanId) throws WalletManagementException;
+    Loan disburseLoan(String loanId) throws WalletManagementException;
+    void repayLoan(String loanId, BigDecimal amount) throws WalletManagementException;
+    Loan getLoanDetails(String loanId) throws WalletManagementException;
+    List<Loan> getAllLoansForUser(String userId) throws WalletManagementException;
+    List<Loan> listAllLoans() throws WalletManagementException;
 }
