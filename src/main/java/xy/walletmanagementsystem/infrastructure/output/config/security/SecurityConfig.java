b48f47fa8c644d9a9c774e0540f737333eda2023
deji <ayodeji2k@gmail.com>
2026-04-17 14:27:01 +0100

    created securityConfig

---
+++ src/main/java/xy/walletmanagementsystem/infrastructure/output/config/security/SecurityConfig.java
@@ -0,0 +1,102 @@
+package xy.walletmanagementsystem.infrastructure.output.config.security;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import xy.walletmanagementsystem.domain.exception.WalletManagementException;
+import xy.walletmanagementsystem.infrastructure.output.config.host.AllowedHost;
+
+@Slf4j
+@Configuration
+@EnableWebSecurity
+@EnableMethodSecurity(securedEnabled = true, jsr250Enabled = true)
+@RequiredArgsConstructor
+public class SecurityConfig {
+  private final CustomAuthenticationEntryPoint authenticationEntryPoint;
+  private final AllowedHost allowedHost;
+  private final JwtAuthenticationFilter jwtAuthenticationFilter;
+
+  @Bean
+  public PasswordEncoder passwordEncoder() {
+    return new BCryptPasswordEncoder();
+  }
+
+  @Bean
+  public AuthenticationManager authenticationManager(AuthenticationConfiguration config) throws WalletManagementException {
+    return config.getAuthenticationManager();
+  }
+
+  @Bean
+  public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
+    httpSecurity
+        .csrf(AbstractHttpConfigurer::disable)
+        .cors(Customizer.withDefaults())
+        .exceptionHandling(
+            exception -> exception.authenticationEntryPoint(authenticationEntryPoint))
+        .authorizeHttpRequests(
+            authorize ->
+                authorize
+                    .requestMatchers("/api/v1/funds-tracker/auth/**")
+                    .permitAll()
+                    .requestMatchers("/api/v1/funds-tracker/admin/**")
+                    .hasAnyRole("ADMIN", "SUPER_ADMIN")
+                    .requestMatchers(HttpMethod.GET, WhiteList.GET_PATTERNS)
+                    .permitAll()
+                    .requestMatchers(HttpMethod.OPTIONS, "/**")
+                    .permitAll()
+                    .anyRequest()
+                    .authenticated())
+        .sessionManagement(
+            session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+        .addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class)
+        .httpBasic(AbstractHttpConfigurer::disable);
+    return httpSecurity.build();
+  }
+
+  // @Bean
+  // public CorsConfigurationSource corsConfigurationSource() {
+  // CorsConfiguration configuration = new CorsConfiguration();
+  // configuration.setAllowedOriginPatterns(allowedHost.getPatterns());
+  // configuration.setAllowedMethods(allowedHost.getMethods());
+  // configuration.setAllowedHeaders(List.of("Authorization", "Content-Type"));
+  // configuration.setAllowCredentials(true);
+  //
+  // UrlBasedCorsConfigurationSource source = new
+  // UrlBasedCorsConfigurationSource();
+  // source.registerCorsConfiguration("/**", configuration);
+  // return source;
+  // }
+
+  @Bean
+  WebMvcConfigurer corsConfigurer() {
+    return new WebMvcConfigurer() {
+      @Override
+      public void addCorsMappings(CorsRegistry registry) {
+        registry
+            .addMapping("/**")
+            .allowedOrigins(allowedHost.getArrayPatterns())
+            .allowedMethods(allowedHost.getArrayMethods())
+            .allowedHeaders("*")
+            .exposedHeaders("*")
+            .allowCredentials(true);
+      }
+    };
+  }
+}
