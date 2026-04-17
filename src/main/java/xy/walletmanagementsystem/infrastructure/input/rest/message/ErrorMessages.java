e23063609da3b7bf4fa72fb7074d1ce37d44580c
deji <ayodeji2k@gmail.com>
2026-04-17 13:25:27 +0100

    created ErrorMessages

---
+++ src/main/java/xy/walletmanagementsystem/infrastructure/input/rest/message/ErrorMessages.java
@@ -0,0 +1,7 @@
+package xy.walletmanagementsystem.infrastructure.input.rest.message;
+
+public class ErrorMessages {
+    public static final String INTERNAL_SERVER_ERROR = "An unexpected error occurred. Please try again later.";
+    public static final String FORBIDDEN = "You do not have permission to access this resource.";
+    public static final String INVALID_INPUT_ERROR_MESSAGE = "Invalid input. Please check your request and try again.";
+}
