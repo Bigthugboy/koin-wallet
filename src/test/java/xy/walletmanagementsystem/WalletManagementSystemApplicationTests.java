440c02af456cf63559103dd4ad50eb3a4579b61e
deji <ayodeji2k@gmail.com>
2026-04-17 09:48:54 +0100

    created WalletManagementSystemApplicationTests

---
+++ src/test/java/xy/walletmanagementsystem/WalletManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package xy.walletmanagementsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WalletManagementSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
