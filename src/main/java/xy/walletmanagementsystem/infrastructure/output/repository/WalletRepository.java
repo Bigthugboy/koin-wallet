b1793d9c86f6ca19a3453ad46df04a5459ba1edd
deji <ayodeji2k@gmail.com>
2026-04-17 16:46:42 +0100

    created walletRepository

---
+++ src/main/java/xy/walletmanagementsystem/infrastructure/output/repository/WalletRepository.java
@@ -0,0 +1,10 @@
+package xy.walletmanagementsystem.infrastructure.output.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import xy.walletmanagementsystem.infrastructure.output.entities.WalletEntity;
+
+import java.util.Optional;
+
+public interface WalletRepository extends JpaRepository<WalletEntity, String> {
+    Optional<WalletEntity> findByUserId(String userId);
+}
