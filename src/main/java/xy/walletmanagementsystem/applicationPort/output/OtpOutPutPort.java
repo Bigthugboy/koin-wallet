416d7cb7a8d014bd3e6032bc576ed82006382445
deji <ayodeji2k@gmail.com>
2026-04-17 16:48:48 +0100

    added loan outPutPort methods

--- src/main/java/xy/walletmanagementsystem/applicationPort/output/OtpOutPutPort.java
+++ src/main/java/xy/walletmanagementsystem/applicationPort/output/OtpOutPutPort.java
@@ -1,4 +1,11 @@
 package xy.walletmanagementsystem.applicationPort.output;
 
+import xy.walletmanagementsystem.domain.model.OtpDetails;
+
+import java.util.Optional;
+
 public interface OtpOutPutPort {
+    OtpDetails save(OtpDetails otpDetails);
+    Optional<OtpDetails> findByEmailAndType(String email, String type);
+    void deleteByEmailAndType(String email, String type);
 }
