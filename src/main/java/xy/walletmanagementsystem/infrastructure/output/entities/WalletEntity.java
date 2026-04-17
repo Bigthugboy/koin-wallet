c2f6586132b18a8af27465cb0e168727920795b7
deji <ayodeji2k@gmail.com>
2026-04-17 16:45:46 +0100

    created walletEntities

---
+++ src/main/java/xy/walletmanagementsystem/infrastructure/output/entities/WalletEntity.java
@@ -0,0 +1,40 @@
+package xy.walletmanagementsystem.infrastructure.output.entities;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import xy.walletmanagementsystem.domain.enums.WalletStatus;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "wallets")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class WalletEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private String id;
+
+    @Column(nullable = false, unique = true)
+    private String userId;
+
+    @Column(nullable = false, precision = 19, scale = 2)
+    private BigDecimal balance;
+
+    @Column(nullable = false)
+    private String currency;
+
+    @Enumerated(EnumType.STRING)
+    private WalletStatus status;
+
+    private LocalDateTime createdDate;
+    private LocalDateTime updatedDate;
+}
