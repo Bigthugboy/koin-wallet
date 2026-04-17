fb02004d6f995f9826d8fd343eb3e14d217daa58
deji <ayodeji2k@gmail.com>
2026-04-17 14:28:28 +0100

    created OpenApiConfig

---
+++ src/main/java/xy/walletmanagementsystem/infrastructure/output/config/swagger/OpenApiConfig.java
@@ -0,0 +1,39 @@
+package xy.walletmanagementsystem.infrastructure.output.config.swagger;
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeIn;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import io.swagger.v3.oas.annotations.security.SecurityScheme;
+import io.swagger.v3.oas.models.OpenAPI;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@OpenAPIDefinition(
+        info = @Info(
+                title = "Wallet Management System API",
+                version = "1.0",
+                description = "REST API documentation for wallet management  application",
+                contact = @Contact(name = "wallet management system")
+        ),
+        security = {
+                @SecurityRequirement(name = "bearerAuth")
+        }
+)
+@SecurityScheme(
+        name = "bearerAuth",
+        description = "JWT authentication",
+        scheme = "bearer",
+        type = SecuritySchemeType.HTTP,
+        bearerFormat = "JWT",
+        in = SecuritySchemeIn.HEADER
+)
+public class OpenApiConfig {
+        @Bean
+        public OpenAPI customOpenAPI() {
+                return new OpenAPI();
+        }
+}
