2b698ff25347a3b353ef5f5a09bd942834f316ec
deji <ayodeji2k@gmail.com>
2026-04-17 16:43:14 +0100

    created transactionResponse

---
+++ src/main/java/xy/walletmanagementsystem/infrastructure/input/rest/data/response/TransactionResponse.java
@@ -0,0 +1,26 @@
+package xy.walletmanagementsystem.infrastructure.input.rest.data.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import xy.walletmanagementsystem.domain.enums.TransactionStatus;
+import xy.walletmanagementsystem.domain.enums.TransactionType;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class TransactionResponse {
+    private String transactionId;
+    private String userId;
+    private String walletId;
+    private TransactionType type;
+    private BigDecimal amount;
+    private TransactionStatus status;
+    private String referenceNumber;
+    private LocalDateTime timestamp;
+}
