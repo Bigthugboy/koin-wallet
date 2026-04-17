1f48bf0021d19f3ee6673370c40572fa8072d631
deji <ayodeji2k@gmail.com>
2026-04-17 09:48:42 +0100

    created WalletManagementSystemApplication

---
+++ src/main/java/xy/walletmanagementsystem/WalletManagementSystemApplication.java
@@ -0,0 +1,13 @@
+package xy.walletmanagementsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WalletManagementSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WalletManagementSystemApplication.class, args);
+    }
+
+}
