a35a30c69c520698e2fa6d8dd16ebd3ab8c151c5
deji <ayodeji2k@gmail.com>
2026-04-18 16:44:17 +0100

    update repo

--- src/main/java/xy/walletmanagementsystem/infrastructure/output/repository/LoanRepository.java
+++ src/main/java/xy/walletmanagementsystem/infrastructure/output/repository/LoanRepository.java
@@ -5,6 +5,6 @@ import xy.walletmanagementsystem.infrastructure.output.entities.LoanEntity;
 
 import java.util.List;
 
-public interface LoanRepository extends JpaRepository<LoanEntity, String> {
+public interface LoanRepository extends JpaRepository<LoanEntity, Long> {
     List<LoanEntity> findByUserId(String userId);
 }
