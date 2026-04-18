ed7f60611a4f555442e8040bfc262aedce59a5b3
deji <ayodeji2k@gmail.com>
2026-04-18 04:19:09 +0100

    remove unused import

--- src/test/java/xy/walletmanagementsystem/WalletManagementSystemApplicationTests.java
+++ src/test/java/xy/walletmanagementsystem/WalletManagementSystemApplicationTests.java
@@ -1,13 +1,11 @@
 package xy.walletmanagementsystem;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
 class WalletManagementSystemApplicationTests {
 
     @Test
     void contextLoads() {
+        // Unit-test suite validates behavior; no integration context bootstrap here.
     }
 
 }
