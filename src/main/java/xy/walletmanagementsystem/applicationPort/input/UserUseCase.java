3cf9974feaa4344ae057a0da37e232de000c9468
deji <ayodeji2k@gmail.com>
2026-04-18 04:18:29 +0100

    add method getUserDetials

--- src/main/java/xy/walletmanagementsystem/applicationPort/input/UserUseCase.java
+++ src/main/java/xy/walletmanagementsystem/applicationPort/input/UserUseCase.java
@@ -3,8 +3,11 @@ package xy.walletmanagementsystem.applicationPort.input;
 import xy.walletmanagementsystem.domain.exception.WalletManagementException;
 import xy.walletmanagementsystem.domain.model.User;
 
+import java.util.Optional;
+
 public interface UserUseCase {
         User createUser(User user);
         User updateUserProfile(String userId, User user) throws WalletManagementException;
 
+        Optional<User> getUserDetails(String id) throws WalletManagementException;
 }
