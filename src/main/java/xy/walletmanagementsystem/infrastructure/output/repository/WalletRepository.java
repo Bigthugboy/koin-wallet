edbbdba6f81a63fef8bf8eed3e72685e2c66df61
deji <ayodeji2k@gmail.com>
2026-04-18 17:19:19 +0100

    update class

--- src/main/java/xy/walletmanagementsystem/infrastructure/output/repository/WalletRepository.java
+++ src/main/java/xy/walletmanagementsystem/infrastructure/output/repository/WalletRepository.java
@@ -6,5 +6,5 @@ import xy.walletmanagementsystem.infrastructure.output.entities.WalletEntity;
 import java.util.Optional;
 
 public interface WalletRepository extends JpaRepository<WalletEntity, Long> {
-    Optional<WalletEntity> findByUserId(String userId);
+    Optional<WalletEntity> findByUserId(Long userId);
 }
