73f116b9a8bc3b52c78e148a833d5f0980833202
deji <ayodeji2k@gmail.com>
2026-04-18 17:19:06 +0100

    update class

--- src/main/java/xy/walletmanagementsystem/infrastructure/input/rest/data/response/UserResponse.java
+++ src/main/java/xy/walletmanagementsystem/infrastructure/input/rest/data/response/UserResponse.java
@@ -14,7 +14,7 @@ import java.time.LocalDateTime;
 @AllArgsConstructor
 @NoArgsConstructor
 public class UserResponse {
-    private String id;
+    private Long id;
     private String fullName;
     private String email;
     private String phoneNumber;
