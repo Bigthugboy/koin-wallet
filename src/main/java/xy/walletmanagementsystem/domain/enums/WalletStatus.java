262c5cc13a262a62d0d45ec4245912bcf7491eb1
deji <ayodeji2k@gmail.com>
2026-04-18 04:54:00 +0100

    update class

--- src/main/java/xy/walletmanagementsystem/domain/enums/WalletStatus.java
+++ src/main/java/xy/walletmanagementsystem/domain/enums/WalletStatus.java
@@ -1,17 +1,9 @@
 package xy.walletmanagementsystem.domain.enums;
 
 public enum WalletStatus {
-    ACTIVE(1),
-    SUSPENDED(2),
-    PENDING(0);
+    ACTIVE,
+    SUSPENDED,
+    PENDING;
 
-    private final int value;
 
-    WalletStatus(int value) {
-        this.value = value;
-    }
-
-    public int getValue() {
-        return value;
-    }
 }
