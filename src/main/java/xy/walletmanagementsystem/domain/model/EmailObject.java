978addf75849e22bd3d7cae70faad4f7d0867377
deji <ayodeji2k@gmail.com>
2026-04-18 04:10:23 +0100

    create emailObject

---
+++ src/main/java/xy/walletmanagementsystem/domain/model/EmailObject.java
@@ -0,0 +1,18 @@
+package xy.walletmanagementsystem.domain.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class EmailObject {
+    private String recipient;
+    private String subject;
+    private String body;
+}
