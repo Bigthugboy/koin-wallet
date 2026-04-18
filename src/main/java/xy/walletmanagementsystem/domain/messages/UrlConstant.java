d4f94a608fb66b7a5963915407884bb3f1413d69
deji <ayodeji2k@gmail.com>
2026-04-18 04:17:06 +0100

    create UrlConstant

---
+++ src/main/java/xy/walletmanagementsystem/domain/messages/UrlConstant.java
@@ -0,0 +1,16 @@
+package xy.walletmanagementsystem.domain.messages;
+
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
+public class UrlConstant {
+    public static final String BASE_URL = "/api/v1/koin-wallet";
+    public static final String KYC_URL = BASE_URL + "/kyc";
+    public static final String AUTH_URL = BASE_URL + "/auth";
+    public static final String USER_URL = BASE_URL + "/users/account";
+    public static final String ADMIN_URL = BASE_URL + "/admin";
+    public static final String TRANSACTION_URL = BASE_URL + "/transaction";
+    public static final String WALLET_URL = BASE_URL + "/wallet";
+    public static final String LOAN_URL = BASE_URL + "/loan";
+}
