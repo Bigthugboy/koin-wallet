d83e4dcc961d8f416e750cbb56d57998fb5c40d8
deji <ayodeji2k@gmail.com>
2026-04-19 15:50:39 +0100

    add toKyc method

--- src/main/java/xy/walletmanagementsystem/infrastructure/input/rest/mapper/RestMapper.java
+++ src/main/java/xy/walletmanagementsystem/infrastructure/input/rest/mapper/RestMapper.java
@@ -9,6 +9,7 @@ import xy.walletmanagementsystem.domain.model.Loan;
 import xy.walletmanagementsystem.domain.model.Transaction;
 import xy.walletmanagementsystem.domain.model.User;
 import xy.walletmanagementsystem.domain.model.Wallet;
+import xy.walletmanagementsystem.infrastructure.input.rest.data.request.KycRequest;
 import xy.walletmanagementsystem.infrastructure.input.rest.data.request.SignupRequest;
 import xy.walletmanagementsystem.infrastructure.input.rest.data.request.UpdateUserRequest;
 import xy.walletmanagementsystem.infrastructure.input.rest.data.response.AuthenticationResponse;
@@ -36,4 +37,6 @@ public interface RestMapper {
     User toUser(@Valid SignupRequest request);
 
     User toUpdateUser(UpdateUserRequest updateUserRequest);
+
+    Kyc toKyc(@Valid KycRequest request,Long userId);
 }
