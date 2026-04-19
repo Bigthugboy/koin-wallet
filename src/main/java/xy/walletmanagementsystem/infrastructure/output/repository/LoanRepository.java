8830f14b566d8ce5ba2de8369e718858c12d2478
deji <ayodeji2k@gmail.com>
2026-04-19 15:47:57 +0100

    add idemopotency key

--- src/main/java/xy/walletmanagementsystem/infrastructure/output/repository/LoanRepository.java
+++ src/main/java/xy/walletmanagementsystem/infrastructure/output/repository/LoanRepository.java
@@ -7,4 +7,5 @@ import java.util.List;
 
 public interface LoanRepository extends JpaRepository<LoanEntity, Long> {
     List<LoanEntity> findByUserId(Long userId);
+    java.util.Optional<LoanEntity> findByIdempotencyKey(String idempotencyKey);
 }
