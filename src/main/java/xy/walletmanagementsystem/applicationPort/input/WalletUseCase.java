933569b2f89eccb4c44e755742c2bbf9c2a9f129
deji <ayodeji2k@gmail.com>
2026-04-19 15:58:16 +0100

    update WalletUseCase

--- src/main/java/xy/walletmanagementsystem/applicationPort/input/WalletUseCase.java
+++ src/main/java/xy/walletmanagementsystem/applicationPort/input/WalletUseCase.java
@@ -2,14 +2,18 @@ package xy.walletmanagementsystem.applicationPort.input;
 
 import xy.walletmanagementsystem.domain.exception.WalletManagementException;
 import xy.walletmanagementsystem.domain.model.Transaction;
+import xy.walletmanagementsystem.domain.model.User;
 import xy.walletmanagementsystem.domain.model.Wallet;
+import xy.walletmanagementsystem.infrastructure.output.paystack.PaystackFundingInitResponse;
 
 import java.math.BigDecimal;
 import java.util.List;
 
 public interface WalletUseCase {
     Wallet createWallet(Long userId) throws WalletManagementException;
-    void fundWallet(Long userId, BigDecimal amount, String reference) throws WalletManagementException;
+    void fundWallet(Long userId, BigDecimal amount, String reference, String idempotencyKey) throws WalletManagementException;
     Wallet getWalletBalance(Long userId) throws WalletManagementException;
     List<Transaction> getTransactionHistory(Long userId) throws WalletManagementException;
+    PaystackFundingInitResponse initializeFunding(User user, BigDecimal amount) throws WalletManagementException;
+    void confirmFunding(String reference, BigDecimal amount, String customerEmail) throws WalletManagementException;
 }
