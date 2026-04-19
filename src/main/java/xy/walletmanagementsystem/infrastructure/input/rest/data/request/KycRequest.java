cadbf297e282226ec46e6430b1f537eb6f5acb7d
deji <ayodeji2k@gmail.com>
2026-04-19 11:57:12 +0100

    update request

--- src/main/java/xy/walletmanagementsystem/infrastructure/input/rest/data/request/KycRequest.java
+++ src/main/java/xy/walletmanagementsystem/infrastructure/input/rest/data/request/KycRequest.java
@@ -11,9 +11,6 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @NoArgsConstructor
 public class KycRequest {
-    @NotBlank(message = "BVN is required")
     private String bvn;
-
-    @NotBlank(message = "NIN is required")
     private String nin;
 }
