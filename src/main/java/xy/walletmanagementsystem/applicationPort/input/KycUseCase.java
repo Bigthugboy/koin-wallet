d0a31c3020f0e4928347af6d209fd9d1a4a22405
deji <ayodeji2k@gmail.com>
2026-04-19 11:57:43 +0100

    update kycUseCase

--- src/main/java/xy/walletmanagementsystem/applicationPort/input/KycUseCase.java
+++ src/main/java/xy/walletmanagementsystem/applicationPort/input/KycUseCase.java
@@ -4,7 +4,7 @@ import xy.walletmanagementsystem.domain.exception.WalletManagementException;
 import xy.walletmanagementsystem.domain.model.Kyc;
 
 public interface KycUseCase {
-    Kyc submitKyc(Long userId, String bvn, String nin) throws WalletManagementException;
+    Kyc submitKyc( Kyc kyc) throws WalletManagementException;
     Kyc getKycDetails(Long userId) throws WalletManagementException;
     Kyc updateVerificationStatus(Long kycId, String status) throws WalletManagementException;
 }
