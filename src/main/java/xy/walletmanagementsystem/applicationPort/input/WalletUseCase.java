c9b892d68eea776eb0860ff327900fac0eb87432
deji <ayodeji2k@gmail.com>
2026-04-19 17:27:22 +0100

    update class

--- src/main/java/xy/walletmanagementsystem/applicationPort/input/WalletUseCase.java
+++ src/main/java/xy/walletmanagementsystem/applicationPort/input/WalletUseCase.java
@@ -1,19 +1,29 @@
 package xy.walletmanagementsystem.applicationPort.input;
 
+import xy.walletmanagementsystem.domain.enums.TransactionStatus;
 import xy.walletmanagementsystem.domain.exception.WalletManagementException;
+import xy.walletmanagementsystem.domain.model.PaystackWebhookEvent;
+import xy.walletmanagementsystem.domain.model.PaystackFundingInitResponse;
 import xy.walletmanagementsystem.domain.model.Transaction;
 import xy.walletmanagementsystem.domain.model.User;
 import xy.walletmanagementsystem.domain.model.Wallet;
-import xy.walletmanagementsystem.infrastructure.output.paystack.PaystackFundingInitResponse;
 
 import java.math.BigDecimal;
 import java.util.List;
 
 public interface WalletUseCase {
+
     Wallet createWallet(Long userId) throws WalletManagementException;
+
     void fundWallet(Long userId, BigDecimal amount, String reference, String idempotencyKey) throws WalletManagementException;
+
     Wallet getWalletBalance(Long userId) throws WalletManagementException;
+
     List<Transaction> getTransactionHistory(Long userId) throws WalletManagementException;
+
     PaystackFundingInitResponse initializeFunding(User user, BigDecimal amount) throws WalletManagementException;
-    void confirmFunding(String reference, BigDecimal amount, String customerEmail) throws WalletManagementException;
+
+    void confirmFunding(PaystackWebhookEvent event) throws WalletManagementException;
+
+    void markTransactionTerminal(String reference, TransactionStatus terminalStatus, String eventType) throws WalletManagementException;
 }
